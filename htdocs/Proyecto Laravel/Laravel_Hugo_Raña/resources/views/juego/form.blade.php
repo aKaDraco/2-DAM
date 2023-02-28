b9c69756fd4f3f0a7479c34c831bcd8a1f0d8835
Hugo_1022 <hugohtt@hotmail.com>
2023-02-28 13:47:44 +0100

    28/02/2023

---
+++ htdocs/Proyecto Laravel/Laravel_Hugo_Raña/resources/views/juego/form.blade.php
@@ -0,0 +1,29 @@
+<div class="box box-info padding-1">
+    <div class="box-body">
+        
+        <div class="form-group">
+            {{ Form::label('categoria') }}
+            {{ Form::select('categoria', $categorias,$juego->categoria_id, ['class' => 'form-control' . ($errors->has('categoria_id') ? ' is-invalid' : ''), 'placeholder' => 'Categoria Id']) }}
+            {!! $errors->first('categoria_id', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('nombre') }}
+            {{ Form::text('nombre', $juego->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
+            {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('valoracion') }}
+            {{ Form::text('valoracion', $juego->valoracion, ['class' => 'form-control' . ($errors->has('valoracion') ? ' is-invalid' : ''), 'placeholder' => 'Valoracion']) }}
+            {!! $errors->first('valoracion', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('precio') }}
+            {{ Form::text('precio', $juego->precio, ['class' => 'form-control' . ($errors->has('precio') ? ' is-invalid' : ''), 'placeholder' => 'Precio']) }}
+            {!! $errors->first('precio', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+
+    </div>
+    <div class="box-footer mt20">
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+</div>
---
+++ htdocs/Proyecto Laravel/Laravel_Hugo_Raña/resources/views/juego/show.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('template_title')
+    {{ $juego->name ?? 'Show Juego' }}
+@endsection
+
+@section('content')
+    <section class="content container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="float-left">
+                            <span class="card-title">Show Juego</span>
+                        </div>
+                        <div class="float-right">
+                            <a class="btn btn-primary" href="{{ route('juegos.index') }}"> Back</a>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        
+                        <div class="form-group">
+                            <strong>Categoria Id:</strong>
+                            {{ $juego->categoria_id }}
+                        </div>
+                        <div class="form-group">
+                            <strong>Nombre:</strong>
+                            {{ $juego->nombre }}
+                        </div>
+                        <div class="form-group">
+                            <strong>Valoracion:</strong>
+                            {{ $juego->valoracion }}
+                        </div>
+                        <div class="form-group">
+                            <strong>Precio:</strong>
+                            {{ $juego->precio }}
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
