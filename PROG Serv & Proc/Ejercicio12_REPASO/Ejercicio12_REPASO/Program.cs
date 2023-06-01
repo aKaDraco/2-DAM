870e82e1c74ac6b5690165d86a4cf5ac736341f6
aKaDraco <113427378+aKaDraco@users.noreply.github.com>
2023-06-01 11:08:26 +0200

    01/06/2023

---
+++ PROG Serv & Proc/Ejercicio12_REPASO/Ejercicio12_REPASO/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio12_REPASO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServidorArchivos s = new ServidorArchivos();
+
+            Console.ReadKey();
+        }
+    }
+}
